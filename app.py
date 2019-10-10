600a4cc78861c935c53ed6ad5e62b24914922b06
soundreaper@gmail.com <soundreaper@gmail.com>
2019-10-09 22:15:41 -0700

    create route for client's 'cart' which contains their choice of starterpack

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ client_cart = db.client_cart
 client_cart.drop
 
 db.starterpacks.insert_many([
-    {"name": "VSCO Girl Starter Pack", "description": "A VSCO girl's starter pack", "price": 200.00, "image": "./static/"},
-    {"name": "College Student Starter Pack", "description": "A College Student's starter pack", "price": "A Lifetime of Debt", "image": "./static/"},
+    {"name": "VSCO Girl Starter Pack", "description": "A VSCO girl's Starter Pack", "price": 200.00, "image": "./static/"},
+    {"name": "College Student Starter Pack", "description": "A College Student's Starter Pack", "price": "A Lifetime of Debt", "image": "./static/"},
     {"name": "CS Student Starter Pack", "description": "A CS Student's Starter Pack", "price": "Your Sanity", "image": "./static/"},
     {"name": "'Pro' Gamer Starter Pack", "description": "A 'Pro' Gamer's Starter Pack", "price": "The Ability to Leave Home", "image": "./static/"}
     ])
@@ -26,6 +26,13 @@ def starterpacks_index():
     """Show all Starterpacks."""
     # This will display all the starterpacks by looping through the DB
     return render_template('starterpacks_index.html', playlists=starterpacks.find())
+
+@app.route('/client_cart')
+def show_client_cart():
+    """Show the client's cart."""
+    cart = client_cart.find()
+    # This will display all the starterpacks by looping through the DB
+    return render_template('cart.html', client_cart=cart)
 
 if __name__ == '__main__':
   app.run(debug=True)
